7d5906e927f52b8ea8f26cf0c71aca3f9319573a
Ariadna Frias <ariadna.812@gmail.com>
2023-09-15 13:04:31 +0200

    Insert search box with service

---
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'shared-search-box',
+  templateUrl: './search-box.component.html',
+})
+export class SearchBoxComponent {
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+  @Input()
+  public placeholder: string = '';
+
+  emitValue(value: string): void {
+    this.onValue.emit(value);
+  }
+}
