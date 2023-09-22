57e6bc12f4e52fa42bbc528f020cacd084e21b15
Ariadna Frias <ariadna.812@gmail.com>
2023-09-22 20:06:11 +0200

    Added permanent feature in info

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { Country } from '../../interfaces/country';
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country.interface';
+import { Region } from '../../interfaces/region.type';
 import { CountriesService } from '../../services/countries.service';
-
-type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: [],
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
+
   public regions: Region[] = [
     'Africa',
     'Americas',
@@ -21,6 +21,11 @@ export class ByRegionPageComponent {
   public selectedRegion?: Region;
   public isLoading: boolean = false;
   constructor(private countriesService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
     this.isLoading = true;
